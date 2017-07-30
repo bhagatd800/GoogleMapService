7156a7c76b534b29de99bec80b66848bb58724fc
kd33pK <bhagatd800@gmail.com>
2017-07-30 11:26:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var app = express();
 
- mongoose.connect('mongodb://healthaditya:4b6U5ptwY3rTIXe8cmiBeHsVpTPFwGDHVMKOu6tN7BqUJSAZDvlBjZkhICg8DgmbXUAvpED2tHeC9Z1uyMygUA==@healthaditya.documents.azure.com:10255/?ssl=true&replicaSet=globaldb');
+ mongoose.openUri('mongodb://healthaditya:4b6U5ptwY3rTIXe8cmiBeHsVpTPFwGDHVMKOu6tN7BqUJSAZDvlBjZkhICg8DgmbXUAvpED2tHeC9Z1uyMygUA==@healthaditya.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',{
+ });
 
 app.set('views', path.join(__dirname, 'views'));
 
